604b60954d82e8ad6e1ac2b7855efdc1011dd5e8
Fatahillah Satria Bima Seno <55565303+fathsbs@users.noreply.github.com>
2019-12-19 07:19:29 +0700

    Add files via upload

---
+++ ME!/src/me/Main.java
@@ -0,0 +1,22 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+package me;
+
+/**
+ *
+ * @author Seno
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
